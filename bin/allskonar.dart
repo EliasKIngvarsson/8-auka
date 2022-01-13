81e2a49b583ff383ba791f39c8ac9e9a3c6a218e
EliasKIngvarsson <iljakarevski@gmail.com>
2022-01-13 20:36:19 +0000

    first commit

---
+++ bin/allskonar.dart
@@ -0,0 +1,12 @@
+
+//write a program that asks the user for two numbers Store the number and print them
+
+import 'dart:io';
+void main(){
+  print('sláðu inn 2 tölur');
+  int t1= int.parse(stdin.readLineSync());
+  int t2= int.parse(stdin.readLineSync());
+
+  print('tala 1 = $t1 tala 2 = $t2');
+
+}
